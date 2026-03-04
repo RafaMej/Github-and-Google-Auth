c5b19964b72c734082c58b97023376dfdf23a322
Rafael <rafaelmejia2005@gmail.com>
2026-03-03 18:25:03 -0600

    Initial Commit

---
+++ StudentApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentApp
+//
+//  Created by Rafael Mejía López on 03/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentApp/StudentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentAppApp.swift
+//  StudentApp
+//
+//  Created by Rafael Mejía López on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
